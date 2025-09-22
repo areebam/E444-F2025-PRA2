20c9624f10cde1d2d7265a3aa270547217b80b3c
areebam <66850968+areebam@users.noreply.github.com>
2025-09-21 23:32:15 -0400

    Activity 1.3 added bootstrap + timestamp

--- hello.py
+++ hello.py
@@ -9,3 +9,9 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return f'<h1>Hello, {name}!</h1>'
+
+# New route for Activity 1.3 (Bootstrap + timestamp)
+@app.route('/')
+def index():
+    return render_template('index.html', name="Areeba", current_time=datetime.utcnow())
+
