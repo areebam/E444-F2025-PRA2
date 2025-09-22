e54a2c96cfde174ca48b56ad5987c280113e3a57
areebam <66850968+areebam@users.noreply.github.com>
2025-09-22 00:03:41 -0400

    activity 1.3 nav bar, time

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/hello')
 def hello():
